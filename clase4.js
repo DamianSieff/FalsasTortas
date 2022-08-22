7a20741340e5bd68aefece3baac3b95c7f5784a9
Damian Sieff <damiansieff@gmail.com>
2022-08-22 14:28:49 -0300

    clase 4

--- clase4.js
+++ clase4.js
@@ -1,1 +1,85 @@
-const palabra = 'perro';
+/* const palabra = 'perro'
+const fin = () => console.log('terminé')
+
+const mostrarLetras = (string, fun) => {
+    for (i = 0; i < string.length; i++) {
+        let letra = string[i]
+        setTimeout(() => {
+            console.log(letra)
+        }, 1000 * i)
+    }
+    setTimeout(() => {
+        fun()
+    }, 1000 * i)
+}
+
+mostrarLetras(palabra, fin) */
+
+function mostrarLetras(palabra,cb){
+    let i = 0
+    const timer = setInterval(() => {
+        if(palabra.length > i){
+            console.log(palabra[i])
+            i++
+        }else{
+            clearInterval(timer)
+            cb()
+        }
+    }, 1000);
+}
+
+const fin = () => console.log('terminé')
+
+setTimeout(() => {mostrarLetras('hola',fin)}, 0);
+setTimeout(() => {mostrarLetras('hola',fin)}, 250);
+setTimeout(() => {mostrarLetras('hola',fin)}, 500);
+
+
+/////////////////////////////////////////////////
+
+/* const menu = {
+    hamburguesa: 2,
+    milanesa: 3,
+    tiramisu: 2,
+    pasta: 4
+}
+
+const productos = Object.keys(menu)
+
+const crearPedido = (producto) => {
+    return new Promise((resolve, reject) => {
+        if (menu[producto] > 0) {
+            menu[producto] -= 1
+            resolve(`${producto} servido`)
+        } else {
+            reject(`${producto} agotado`)
+        }
+    })
+}
+
+const atenderCliente = (num) => {
+    const pedido = productos[Math.floor(Math.random() * productos.length)]
+
+    crearPedido(pedido)
+        .then((data) => console.log(`Mostrador ${num} - [THEN]`, data))
+        .catch((error) => console.error(`Mostrador ${num} - [CATCH]`, error))
+        .finally(() => {
+            console.log(
+                `Mostrador ${num} - [FINALLY] Orden finalizada`,
+                new Date()
+            )
+            console.log("menu restante:")
+            console.table(menu)
+        })
+}
+
+function habilitarMostrador(num) {
+    const atencion = setInterval(atenderCliente, 2000)
+
+    setTimeout(() => {
+        clearInterval(atencion)
+    }, 5000);
+}
+
+habilitarMostrador(1)
+habilitarMostrador(2) */
---
+++ fs.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+
+/* console.log('iniciado')
+const data = fs.readFileSync("usuario.json", "utf-8")
+console.log(data)
+console.log('terminado')
+
+fs.writeFileSync("usuario.json", JSON.stringify({usuario: "Maria"}) , "utf-8")
+
+const data2 = fs.readFileSync("usuario2.json", "utf-8")
+console.log(data2) */
+
+// fs.appendFileSync("usuario.json", ' Asi agrega appendFileSync')
+
+fs.writeFileSync("usuario2.json",
+    JSON.stringify({ usuario: "Maria" }), "utf-8")
+
+// fs.unlinkSync("usuario2.json")
